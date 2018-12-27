90b699ce5e33660af94935be81127087be458ce0
杨晓晨 <yangxiaochen@cloutropy.com>
2018-12-27 19:40:06 +0800

    添加地址和天气

--- miniprogram/pages/add/index.js
+++ miniprogram/pages/add/index.js
@@ -1,4 +1,6 @@
 // miniprogram/pages/add/index.js
+var amapFile = require('../../libs/amap-wx.js');
+
 Page({
 
   /**
@@ -6,16 +8,52 @@ Page({
    */
   data: {
     message: "",
-    title: ""
+    title: "",
+    address: "",
+    weather: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
+  onChangeAddress: function (event) {
+    // 需要手动对 checked 状态进行更新
+    var that = this
+    // this.setData({ checked: event.detail });
+    var myAmapFun = new amapFile.AMapWX({ key: 'ef26004f24e34015a0e3b64576c1731c' });
+    myAmapFun.getRegeo({
+      success: function (data) {
+        that.setData({
+          address: data[0].regeocodeData.formatted_address
+        })
+      },
+      fail: function (info) {
+        //失败回调
+        console.log(info)
+      }
+    })
+  },
+
+  onChangeWeather: function (event) {
+    var that = this;
+    var myAmapFun = new amapFile.AMapWX({ key: 'ef26004f24e34015a0e3b64576c1731c' });
+    myAmapFun.getWeather({
+      success: function (data) {
+        console.log(data)
+        that.setData({
+          weather: data.weather.data + ' ' + data.winddirection.data + ' ' + data.temperature.data + '°C' + ' 湿度: ' + data.humidity.data
+        })
+      },
+      fail: function (info) {
+        //失败回调
+        console.log(info)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
