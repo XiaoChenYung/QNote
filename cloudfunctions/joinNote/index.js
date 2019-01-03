17cfd7060e4f81f4e4357815697a46cbb644aa4a
杨晓晨 <yangxiaochen@cloutropy.com>
2019-01-03 10:34:16 +0800

    调试云函数

--- cloudfunctions/joinNote/index.js
+++ cloudfunctions/joinNote/index.js
@@ -7,11 +7,11 @@ const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  const {user, nID} = event
+  const {user, nID, formID} = event
   try {
     let note = await db.collection('note').doc(nID).get()
     let openID = wxContext.OPENID
-    let friends = note.friends
+    let friends = note.data.friends
     let temp = 0
     for(var i = 0; i < friends.length; i++) {
       if(friends[i].open_id == openID) {
@@ -28,17 +28,26 @@ exports.main = async (event, context) => {
         friends: friends
       }
     })
-    if (result.updated == 1) {
-      note.type = 2
-      note.frients = []
+    if (result.stats.updated == 1) {
       let newNID = await db.collection('note').add({
-        data: note
+        data: {
+          title: note.data.title,
+          detail: note.data.detail,
+          c_date: note.data.c_date,
+          a_date: note.data.a_date,
+          address: note.data.address,
+          weather: note.data.weather,
+          form_id: formId,
+          status: 1, // 进行中
+          type: 2, // 自己创建的
+          friends: []
+        }
       })
       return {
-        code: -1,
+        code: 0,
         message: "加入成功",
         data: {
-          id: newNID
+          id: newNID.data
         }
       }
     } else {
--- miniprogram/pages/share/index.js
+++ miniprogram/pages/share/index.js
@@ -142,9 +142,7 @@ function addShareUser(that, user, nID) {
       nID: nID
     },
     success: res => {
-      console.log('[云函数] [login] user openid: ', res.result.openid)
-      app.globalData.openID = res.result.openid
-      refreshData(that, options.id)
+      console.log(res)
     },
     fail: err => {
       console.error('[云函数] [login] 调用失败', err)
